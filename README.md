f5cc181fcc2b6d3c417f0aea0b3d793a321dabc3
Rob Patro <robp@ocean.compbio.cs.cmu.edu>
2014-07-18 10:35:36 -0400

    Trying to add travis-ci to master branch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/kingsfordgroup/sailfish.svg?branch=master)](https://travis-ci.org/kingsfordgroup/sailfish)
+
 Setting up Sailfish
 ===================
 
--- cmake/SimpleTest.cmake
+++ cmake/SimpleTest.cmake
@@ -7,7 +7,7 @@ if (TAR_RESULT)
     message(FATAL_ERROR "Error untarring sample_data.tgz")
 endif()
 
-set(INDEX_CMD ${TOPLEVEL_DIR}/bin/sailfish index -t transcripts.fasta -k 20 -o sample_index)
+set(INDEX_CMD ${TOPLEVEL_DIR}/build/src/sailfish index -t transcripts.fasta -k 20 -o sample_index)
 execute_process(COMMAND ${INDEX_CMD}
                 WORKING_DIRECTORY ${TOPLEVEL_DIR}/sample_data
                 RESULT_VARIABLE INDEX_RESULT
@@ -17,7 +17,7 @@ if (INDEX_RESULT)
     message(FATAL_ERROR "Error running ${INDEX_COMMAND}")
 endif()
 
-set(QUANT_COMMAND ${TOPLEVEL_DIR}/bin/sailfish quant -i sample_index --no_bias_correct -l "T=PE:O=><:S=U" -1 reads_1.fastq -2 reads_2.fastq -o sample_quant)
+set(QUANT_COMMAND ${TOPLEVEL_DIR}/build/src/sailfish quant -i sample_index --no_bias_correct -l "T=PE:O=><:S=U" -1 reads_1.fastq -2 reads_2.fastq -o sample_quant)
 execute_process(COMMAND ${QUANT_COMMAND}
 	            WORKING_DIRECTORY ${TOPLEVEL_DIR}/sample_data
 	            RESULT_VARIABLE QUANT_RESULT
