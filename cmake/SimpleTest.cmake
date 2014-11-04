079eccf09e6226ed8dc0e4b8c8db3f892639bf34
Rob Patro <robp@ocean.compbio.cs.cmu.edu>
2014-11-03 23:53:39 -0500

    trying to figure out why test fails remotely

--- cmake/SimpleTest.cmake
+++ cmake/SimpleTest.cmake
@@ -32,29 +32,4 @@ else()
 	message(FATAL_ERROR "Sailfish failed to produce output")
 endif()
 
-set(SALMON_INDEX_CMD ${TOPLEVEL_DIR}/build/src/salmon index -t transcripts.fasta -i sample_salmon_index)
-execute_process(COMMAND ${SALMON_INDEX_CMD}
-                WORKING_DIRECTORY ${TOPLEVEL_DIR}/sample_data
-                RESULT_VARIABLE SALMON_INDEX_RESULT
-                )
-
-if (SALMON_INDEX_RESULT)
-    message(FATAL_ERROR "Error running ${SALMON_INDEX_COMMAND}")
-endif()
-
-set(SALMON_QUANT_COMMAND ${TOPLEVEL_DIR}/build/src/salmon quant -i sample_salmon_index -l IU -1 reads_1.fastq -2 reads_2.fastq -o sample_salmon_quant -n 1000000)
-execute_process(COMMAND ${SALMON_QUANT_COMMAND}
-	            WORKING_DIRECTORY ${TOPLEVEL_DIR}/sample_data
-                RESULT_VARIABLE SALMON_QUANT_RESULT
-                )
-if (SALMON_QUANT_RESULT)
-    message(FATAL_ERROR "Error running ${QUANT_RESULT}")
-endif()
-
-if (EXISTS ${TOPLEVEL_DIR}/sample_data/sample_quant/quant.sf)
-	message("Sailfish ran successfully")
-else()
-	message(FATAL_ERROR "Sailfish failed to produce output")
-endif()
-
 
